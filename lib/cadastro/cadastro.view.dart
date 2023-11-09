66e1f4fd0739e9b97dd4a9d657d0359d0894e406
Alexandre Salgado <imxderr@gmail.com>
2023-11-09 20:06:41 -0300

    update cadastro

--- lib/cadastro/cadastro.view.dart
+++ lib/cadastro/cadastro.view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pac/cadastro/widget/button.widget.dart';
 import 'package:pac/cadastro/widget/checkbox.widget.dart';
 
 class CadastroView extends StatefulWidget {
@@ -12,145 +11,139 @@ class CadastroView extends StatefulWidget {
 class _CadastroViewState extends State<CadastroView> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Cadastro'),
-        backgroundColor: Colors.green[900],
-      ),
-
-      body: _body(),
-    );
-  }
-  
-  _body() {
-    return Container(
-
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Colors.white, Color.fromRGBO(118, 166, 68, 1)],
-          stops: [0.0,0.7], // Cores do degradê
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cadastro'),
+          backgroundColor: Colors.green[900],
         ),
-      ),
-
-      child: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(12),
-          children: const [
-            SizedBox(height:100),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-              'Digite seus dados abaixo para criar sua conta:', 
-              style: TextStyle(
-              fontSize: 15, 
-              fontWeight: FontWeight.normal,
-              ), 
-              
-              ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.white, Color.fromRGBO(118, 166, 68, 1)],
+              stops: [0.0, 0.7],
             ),
-            SizedBox(height: 40),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                  
-                  hintText: 'Nome Completo',
-                  enabled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 255, 255, 255)
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 14, 64, 6),
+          ),
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(22),
+              children: [
+                const SizedBox(height: 100),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'Digite seus dados abaixo para criar sua conta:',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
                     ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  
-                  hintText: 'Email',
-                  enabled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 255, 255, 255)
+                const SizedBox(height: 40), 
+                const TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    hintText: 'Nome Completo',
+                    enabled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 14, 64, 6),
+                      ),
                     ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 14, 64, 6),
+                ), 
+                const SizedBox(height: 10),
+                const TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    enabled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 14, 64, 6),
+                      ),
+                    ),
+                  ),
+                ),       
+                const SizedBox(height: 10),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Senha',
+                    enabled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 14, 64, 6),
+                      ),
+                    ),
+                  ),
+                ), 
+                const SizedBox(height: 10),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Confirme sua Senha',
+                    enabled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 14, 64, 6),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  
-                  hintText: 'Senha',
-                  enabled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 255, 255, 255),
+                const SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: TermosCheck(),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green[900],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 14, 64, 6),
+                  child: const Text(
+                    "Finalizar Cadastro",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 234, 234, 234),
+                      decoration: TextDecoration.none,
                     ),
                   ),
                 ),
-              ),
+                const SizedBox(height: 330),
+              ],
             ),
-            SizedBox(height: 10),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  
-                  hintText: 'Confirme sua Senha',
-                  enabled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 14, 64, 6),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50), 
-              child: TermosCheck(),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 110), 
-              child: BotaoCadastro(),
-            ),
-            
-            SizedBox(height: 330),
-            
-          ],    
+          ),
         ),
       ),
     );
-     
   }
 }

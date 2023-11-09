05ef3d165391331f23f2f44f1ebd2ff4086df0ea
Alexandre Salgado <imxderr@gmail.com>
2023-11-09 19:15:42 -0300

    updated

--- lib/login/login.view.dart
+++ lib/login/login.view.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pac/login/widgets/email.widget.dart';
-import 'package:pac/login/widgets/forgetpassword.widget.dart';
-import 'package:pac/login/widgets/loginbutton.widget.dart';
-import 'package:pac/login/widgets/logo.widget.dart';
-import 'package:pac/login/widgets/password.widget.dart';
+import 'package:pac/cadastro/cadastro.view.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -15,51 +11,114 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-  
-    body: _body(),
-
-    );
-  }
-
-  _body() {
-    return Container(
-    
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Colors.white, Color.fromRGBO(118, 166, 68, 1)],
-          stops: [0.0,0.7], // Cores do degradê
+    return  MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.white, Color.fromRGBO(118, 166, 68, 1)],
+              stops: [0.0, 0.7],
+            ),
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(height: 80),
+                  Image.asset(
+                    "assets/images/logo.png",
+                    width: 100,
+                    height: 100,
+                  ),
+                  const SizedBox(height: 60),
+                  const TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        //labelText: 'Email',
+                        hintText: 'Email',
+                        enabled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 255, 255, 255)
+                          ),
+                        ),
+                          focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 14, 64, 6),
+                          ),
+                        ),
+                      ),
+                  ),
+                  const SizedBox(height: 10),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      //labelText: 'Senha',
+                      hintText: 'Senha',
+                      enabled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 14, 64, 6),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  Row( // Utilize um Row para colocar os botões lado a lado
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          // Ação para o botão "ENTRAR"
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          backgroundColor: Colors.green[900],
+                        ).copyWith(
+                          minimumSize: MaterialStateProperty.all(const Size(0, 40)),
+                          padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 45.0)),
+                        ),
+                        child: const Text('ENTRAR'),
+                      ),
+                      const SizedBox(width: 10), // Espaçamento entre os botões
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const CadastroView(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          backgroundColor: Colors.green[900],
+                        ).copyWith(
+                          minimumSize: MaterialStateProperty.all(const Size(0, 40)),
+                          padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 30.0)),
+                        ),
+                        child: const Text('CRIAR CONTA'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
-
-      child: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(12),
-          children: const [
-            SizedBox(height: 110),
-            LogoIcon(),
-            SizedBox(height: 100),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-            child: EmailField(),
-            ),
-            SizedBox(height: 10),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-            child: PasswordField(),
-            ),
-            SizedBox(height: 17),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40), 
-              child: LoginButtonField(),
-            ),
-            SizedBox(height: 10),
-            ForgetPasswordField(),
-          ],    
-        ),
-      ),
     );
-    
   }
 }
